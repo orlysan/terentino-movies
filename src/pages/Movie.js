4bc3e95dd17d7249eb6e2baefc1b015823c746cc
dm3mak <ianakazantipa@gmail.com>
2021-05-31 21:12:19 +0300

    Using Data, "IF" logic for "Loading", new css file

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 import { withRouter } from "react-router";
+import "./Movie.css"
 class Movie extends React.Component{
     constructor(props){
         super(props);
@@ -53,46 +55,79 @@ class Movie extends React.Component{
             })
     })
 }
-
-
-
-
-
-    //     const promiseArr = [fetch (`https://api.themoviedb.org/3/find/${this.id}?api_key=471a32c8fb5a40f61dbbe1e382bb1a79&language=en-US&external_source=imdb_id`)
-    //     .then((res)=>res.json()), fetch (`http://www.omdbapi.com/?apikey=bdf00e81&i=${this.id}`)
-    //     .then((res)=>res.json())]
-    // //    fetch (`https://api.themoviedb.org/3/find/${this.id}?api_key=471a32c8fb5a40f61dbbe1e382bb1a79&language=en-US&external_source=imdb_id`)
-    // //     .then((res)=>res.json())
-    // Promise.all(promiseArr).then((res)=> {
-    //         const tmdbData = res[0].movie_results[0];
-    //         const omdbData = res[1];
-    //         console.log(omdbData.Ratings[1].Value)
-    //         const movieObj = {
-    //             // title: tmdbData.original_title,
-    //             // img: tmdbData.poster_path,
-    //             // id: tmdbData.id,
-    //             // popularity: tmdbData.popularity,
-    //             // tmdbRaiting: tmdbData.vote_average,
-    //             rottenTomatoes : omdbData.Ratings[1].Value,
-    //             imdbRating : omdbData.imdbRating,
-    //             Metacritic : omdbData.Ratings[1].Value,
-    //             language: tmdbData.original_language,
-    //             actors : omdbData.Actors,
-    //             director : omdbData.Director,
-    //             genres : omdbData.Genre,
-    //             plot : omdbData.Plot,
-    //             writer : omdbData.Writer, 
-    //         }
-    //         this.setState({
-    //             movie: movieObj
-    //         })
-    //     })    
-    // }
     render() {
+        let template = '';
+        if (this.state.tmdbInfo === undefined||this.state.omdbInfo === undefined ){
+             template = <div>loading</div>
+        }
+        else{
+            template = (
+                        <div>
+                            <div className="jumbotron">
+                                <h1 className="display-4 text-center">{this.state.tmdbInfo.title}</h1>
+                                <p className="lead text-center">{this.state.tmdbInfo.tagline}</p>
+                                <hr className="my-3"/>
+                                <p>Directed by: {this.state.omdbInfo.director} </p>
+                            </div>
+                            <div className="container ">
+                                <Row>
+                                    <Col lg={6} className="text-center">
+                                        <img src={`https://image.tmdb.org/t/p/w400${this.state.tmdbInfo.image}`} alt={this.state.tmdbInfo.title}/>
+                                    </Col>
+                                    <Col lg={6}>
+                                    <dl className="row">
+                                        <dt className="col-sm-3">Genres:</dt>
+                                        <dd className="col-sm-9">{this.state.omdbInfo.genres}</dd>
+
+                                        <dt className="col-sm-3">About:</dt>
+                                        <dd className="col-sm-9">
+                                            <p>{this.state.omdbInfo.plot}</p>
+                                        </dd>
+                                        <dt className="col-sm-3">Runtime: </dt>
+                                        <dd className="col-sm-9">{this.state.omdbInfo.runTime}</dd>
+                                        <dt className="col-sm-3">Writers: </dt>
+                                        <dd className="col-sm-9">{this.state.omdbInfo.writer}</dd>
+                                        <dt className="col-sm-3">Actors: </dt>
+                                        <dd className="col-sm-9">{this.state.omdbInfo.actors}</dd>
+                                        <dt className="col-sm-3">Language: </dt>
+                                        <dd className="col-sm-9">{this.state.omdbInfo.language}</dd>
+                                        <dt className="col-sm-3">Year: </dt>
+                                        <dd className="col-sm-9">{this.state.omdbInfo.year}</dd>
+
+                                        <dt className="col-sm-3">Raitings</dt>
+                                        <dd className="col-sm-9">
+                                            <dl className="row">
+                                            <dt className="col-sm-4">IMDB:</dt>
+                                            <dd className="col-sm-8">{this.state.tmdbInfo.tmdbRaiting}</dd>
+                                            </dl>
+                                            <dl className="row">
+                                            <dt className="col-sm-4">TMDB:</dt>
+                                            <dd className="col-sm-8">{this.state.omdbInfo.imdbRating}</dd>
+                                            </dl>
+                                            <dl className="row">
+                                            <dt className="col-sm-4">Rotten Tomatoes:</dt>
+                                            <dd className="col-sm-8">{this.state.omdbInfo.rottenTomatoes}</dd>
+                                            </dl>
+                                            <dl className="row">
+                                            <dt className="col-sm-4">Metacritic:</dt>
+                                            <dd className="col-sm-8">{this.state.omdbInfo.metacritic}</dd>
+                                            </dl>
+                                        </dd>
+                                    </dl>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <div className ="text-center mt-5 iframe-container">
+                                        <h5>Trailer</h5>
+                                    <iframe width="1280" height="720" src={`https://www.youtube.com/embed/${this.state.tmdbInfo.trailer}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                    </div>
+                                </Row>
+                            </div>
+                        </div>
+            )
+        }
         return (<div className="p-movie">
-           
-            <h1>{this.state.tmdbInfo.title}</h1>
-            <iframe width="560" height="315" src={`https://www.youtube.com/embed/${this.state.tmdbInfo.trailer}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                {template}
             </div>)
     }
 }
