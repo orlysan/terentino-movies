5478e87d6cd1e6bf6f4163bb7413a763b40eb700
SaraiRoiz-Git <53699451+SaraiRoiz-Git@users.noreply.github.com>
2021-06-02 14:35:28 +0300

    back

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import NewestMovies from './pages/NewestMovies';
 import SearchResult from './pages/SearchResult';
 import { HashRouter, Route } from 'react-router-dom';
 import NavbarMovies from './components/NavbarMovies';
-import ImageCarusel from './components/ImageCarusel';
 
 
 class App extends React.Component {
@@ -22,9 +21,32 @@ class App extends React.Component {
     }
 
     render() {
-        return <ImageCarusel>
+        return <HashRouter>
 
-        </ImageCarusel>
+            <NavbarMovies />
+
+            <Route exact path="/">
+                <Home></Home>
+            </Route>
+            <Route exact path="/about">
+                <About></About>
+            </Route>
+            <Route exact path="/advanced-search">
+                <AdvancedSearch></AdvancedSearch>
+            </Route>
+            <Route exact path="/highest">
+                <Highest></Highest>
+            </Route>
+            <Route exact path="/movie/:id">
+                <Movie></Movie>
+            </Route>
+            <Route exact path="/newest-movies">
+                <NewestMovies></NewestMovies>
+            </Route>
+            <Route exact path="/search-result">
+                <SearchResult></SearchResult>
+            </Route>
+        </HashRouter>
     }
 }
 
