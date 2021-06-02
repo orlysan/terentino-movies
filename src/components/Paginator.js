1b2008c1578af31bbb86f8630854934aa23e0f28
SaraiRoiz-Git <53699451+SaraiRoiz-Git@users.noreply.github.com>
2021-06-02 23:03:52 +0300

    fix logic

--- src/components/Paginator.js
+++ src/components/Paginator.js
@@ -2,108 +2,112 @@ import React from 'react';
 import { Pagination } from 'react-bootstrap';
 import './Paginator.css';
 
-class Paginator extends React.Component{
-    constructor(props){
+class Paginator extends React.Component {
+    constructor(props) {
         super(props);
-    
+
     }
 
- choosePage = (num) => {
-    this.props.choosePage(num)
-}
-
-firstPage = () => {
-    this.props.choosePage(1)
-}
-
-prevPage = () =>{
-    const current = this.props.currentPage;
-    if(current === 1){
-        return
+    choosePage = (num) => {
+        this.props.choosePage(num)
     }
-    else {
-        this.props.choosePage(parseInt(current - 1))
+
+    firstPage = () => {
+        this.props.choosePage(1)
     }
-}
 
-nextPage = () =>{
-    const current = this.props.currentPage;
-    if(current === this.props.numOfPages){
-        return 
+    prevPage = () => {
+        const current = this.props.currentPage;
+        if (current === 1) {
+            return
+        }
+        else {
+            this.props.choosePage(parseInt(current - 1))
+        }
     }
-    else {
-        this.props.choosePage(parseInt(current + 1))
+
+    nextPage = () => {
+        const current = this.props.currentPage;
+        if (current === this.props.numOfPages) {
+            return
+        }
+        else {
+            this.props.choosePage(parseInt(current + 1))
+        }
     }
-}
 
-lastPage = () =>{
-    this.props.choosePage(this.props.numOfPages)
-}
+    lastPage = () => {
+        this.props.choosePage(this.props.numOfPages)
+    }
 
 
-    render(){
-       const firstEl = this.props.currentPage > 2  ?   <Pagination.First firstLabel="" onClick={this.firstPage}/> : null;
-       const prevEl = this.props.currentPage > 1  ?  <Pagination.Prev onClick={this.prevPage}/> : null;
-       const nextEl = this.props.currentPage <  this.props.numOfPages ?  <Pagination.Next onClick={this.nextPage}/> : null;
-       const lastEl = this.props.currentPage <  this.props.numOfPages-1 ?  <Pagination.Last onClick={this.lastPage}/> : null;
+    render() {
+        const firstEl = this.props.currentPage > 2 ? <Pagination.First firstLabel="" onClick={this.firstPage} /> : null;
+        const prevEl = this.props.currentPage > 1 ? <Pagination.Prev onClick={this.prevPage} /> : null;
+        const nextEl = this.props.currentPage < this.props.numOfPages ? <Pagination.Next onClick={this.nextPage} /> : null;
+        const lastEl = this.props.currentPage < this.props.numOfPages - 1 ? <Pagination.Last onClick={this.lastPage} /> : null;
 
-       
+
         let pages = [];
         let currentPage = this.props.currentPage;
         let numOfPages = this.props.numOfPages;
+        console.log("pages", pages)
+        console.log("currentPage", currentPage)
+        console.log("numOfPages", numOfPages)
 
-        if(currentPage === numOfPages){
-            for(let i = currentPage-2; i = numOfPages; i++){
+        if (currentPage === numOfPages) {
+            console.log("currentPage === numOfPages")
+            for (let i = currentPage - 2; i <= numOfPages; i++) {
                 pages.push(i);
             }
         }
 
-        if(currentPage <= 2 ){
-                for(let i = 1; i<= 3; i++){
-                    pages.push(i);
-                }
+        else if (currentPage <= 2) {
+            for (let i = 1; i <= 3; i++) {
+                pages.push(i);
             }
+        }
 
-            else if(currentPage >= 3){
-                for(let i =currentPage-1; i<= currentPage+1; i++){
-                    pages.push(i);
-                }
+        else if (currentPage >= 3) {
+            for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+                pages.push(i);
             }
- 
+        }
 
-            else {
-                for(let i = currentPage-2; i = numOfPages+1; i++){
-                    pages.push(i);
-                }
+
+        else {
+            for (let i = currentPage - 2; i = numOfPages + 1; i++) {
+                pages.push(i);
             }
+        }
 
 
-           
-            
-         
+
+
+
         // i = should not be 1, instead currentPage - 1
         // We need to make sure we are not displaying page under 1
         // we need to make sure we are not displaying a page that does not exist
-       
+
         return (
             <div className="paginator-container">
-            <Pagination>
-                {firstEl}
-               {prevEl}
-               {pages.map(number => (
-                       <Pagination.Item 
-                            key={number} 
+                <Pagination>
+                    {firstEl}
+                    {prevEl}
+                    {pages.map(number => (
+                        <Pagination.Item
+                            key={number}
                             activeLabel=""
-                            active={number === this.props.currentPage} 
+                            active={number === this.props.currentPage}
                             className="sr-only"
-                            onClick={()=> this.choosePage(number)}>
-                               {number} 
-                       </Pagination.Item>
-                   ))}
-                {nextEl}
-                {lastEl}
-           </Pagination>
-           </div>
+                            onClick={() => this.choosePage(number)}>
+                            {number}
+                        </Pagination.Item>
+                    ))}
+                    {nextEl}
+                    {lastEl}
+                </Pagination>
+            </div>
         )
     }
 }
