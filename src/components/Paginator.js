6ece82db327a5ac42814c4c4cbec675cd85efe97
orly <orlysan1@gmail.com>
2021-06-03 11:15:52 +0300

    adding while loop

--- src/components/Paginator.js
+++ src/components/Paginator.js
@@ -48,38 +48,52 @@ class Paginator extends React.Component {
         const lastEl = this.props.currentPage < this.props.numOfPages - 1 ? <Pagination.Last onClick={this.lastPage} /> : null;
 
 
-        let pages = [];
-        let currentPage = this.props.currentPage;
-        let numOfPages = this.props.numOfPages;
-        console.log("pages", pages)
-        console.log("currentPage", currentPage)
-        console.log("numOfPages", numOfPages)
-
-        if (currentPage === numOfPages) {
-            console.log("currentPage === numOfPages")
-            for (let i = currentPage - 2; i <= numOfPages; i++) {
-                pages.push(i);
+        const pages = [];
+        const currentPage = this.props.currentPage;
+        const numOfPages = this.props.numOfPages;
+      
+        //create the paginator
+    
+        let count = 0;
+        let pageToSet = currentPage <=1 ? currentPage : currentPage-1;
+        while(count <= 3 && pageToSet < numOfPages){
+            if(pageToSet > 0){
+                count++;
+                pages.push(pageToSet);
             }
+            this.pageToTest++
         }
+        console.log(pages )
+        // if (currentPage === numOfPages) {
+        //     for (let i = currentPage - 2; i <= numOfPages; i++) {
+        //         pages.push(i);
+        //     }
+        // }
 
-        else if (currentPage <= 2) {
-            for (let i = 1; i <= 3; i++) {
-                pages.push(i);
-            }
-        }
+        // else if (currentPage <= 2) {
+        //     for (let i = 1; i <= 3; i++) {
+        //         pages.push(i);
+        //     }
+        // }
 
-        else if (currentPage >= 3) {
-            for (let i = currentPage - 1; i <= currentPage + 1; i++) {
-                pages.push(i);
-            }
-        }
+        // else if (currentPage >= 3) {
+        //     for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+        //         pages.push(i);
+        //     }
+        // }
+
+        // else if (currentPage === numOfPages && numOfPages > 2) {
+        //     for (let i = currentPage - 2; i <= numOfPages; i++) {
+        //         pages.push(i);
+        //     }
+        // }
 
 
-        else {
-            for (let i = currentPage - 2; i = numOfPages + 1; i++) {
-                pages.push(i);
-            }
-        }
+        // else {
+        //     for (let i = currentPage - 2; i = numOfPages + 1; i++) {
+        //         pages.push(i);
+        //     }
+        // }
 
 
         return (
